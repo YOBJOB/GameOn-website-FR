6a4700b9512c8e121e41d15d88234d1ddf2a548f
Yoann <92105006+YOBJOB@users.noreply.github.com>
2022-02-09 17:06:31 +0100

    fermeture croix de formulaire d'inscription

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,15 +9,28 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+// page d'inscription
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closemodal = document.querySelector(".close"); 
+// croix de fermeture
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// click sur "je m'inscrit" ouverture modal
+closemodal.addEventListener("click", closeConfirm); 
+// click sur croix fermeture
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+// ouverture du modal
+
+function closeConfirm() {
+  modalbg.style.display = "none";
+} 
+// fermeture du modal
+
 
 
