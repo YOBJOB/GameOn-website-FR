65afa972d14a2f770e7e68c0e26fc584d8d5a3e1
Yoann <92105006+YOBJOB@users.noreply.github.com>
2022-02-15 16:27:21 +0100

    prenom et nom change de couleur et prend en compte >1 lettre

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,20 +8,28 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-// page d'inscription
+const modalbg = document.querySelector(".bground"); //On pointe sur la page d'inscription
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closemodal = document.querySelector(".close"); 
-// croix de fermeture
+const closeModal = document.querySelector(".close");  //On pointe sur la croix de fermeture
+const firstName = document.getElementById("first"); //On pointe sur l'input prénom
+const lastName = document.getElementById("last"); //On pointe sur l'input nom
+const emailUser = document.getElementById("email"); //On pointe sur l'input email
+const birthdateUser = document.getElementById("birthdate"); //On pointe sur l'input anniversaire
+
+
+//DOM Element erreur
+const errorFirst = document.getElementById("error-first"); // Erreur prénom
+const errorLast = document.getElementById("error-last"); // Erreur nom
+
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-//Si click sur "btn je m'inscrit" ouverture modal
-closemodal.addEventListener("click", closeConfirm); 
-//Si click sur croix fermeture utilise fonction CloseConfirm
-
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal)); //Si click sur "btn je m'inscrit" ouverture modal
+closeModal.addEventListener("click", closeConfirm); //Si click sur croix fermeture utilise fonction CloseConfirm
+firstName.addEventListener("change", firstValidation); // Si changement sur prénom utilise fonction firstValidation
+lastName.addEventListener("change", lastValidation); // Si changement sur nom utilise fonction lastValidation
 // launch modal form
+
 function launchModal() {
   modalbg.style.display = "block";
 }
@@ -33,4 +41,29 @@ function closeConfirm() {
 // fermeture du modal
 
 
+function firstValidation() {
+  if (firstName.value.length > 1){ // Si le nombre de caractères est > 1                                {
+      firstName.style.border = "3px solid #279E7A"; // La bordure devient verte
+      errorFirst.style.display = "none"                              
+      return true // Input prénom valide
+    } else {
+      errorFirst.style.display = "block"
+      errorFirst.innerHTML = "Votre prénom doit contenir au moins 2 caractères"
+      firstName.style.border = "3px solid #FE142F"; // La bordure devient rouge
+      return false; // Input prénom invalide
+    }
+  }
+
+  function lastValidation() {
+    if (lastName.value.length > 1){ // Si le nombre de caractères est > 1                                {
+        lastName.style.border = "3px solid #279E7A"; // La bordure devient verte
+        errorLast.style.display = "none"                              
+        return true // Input nom valide
+      } else {
+        errorLast.style.display = "block"
+        errorLast.innerHTML = "Votre prénom doit contenir au moins 2 caractères"
+        lastName.style.border = "3px solid #FE142F"; // La bordure devient rouge
+        return false; // Input nom invalide
+      }
+    }
 
